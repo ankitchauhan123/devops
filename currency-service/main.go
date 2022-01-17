e50cd85c5e6d6a9fc1207140958d2cd81e1149c6
Ankit Chauhan <ankit.c7@gmail.com>
2022-01-17 09:29:25 +0530

    added graceful shutdown

--- currency-service/main.go
+++ currency-service/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
+	"flag"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/gorilla/mux"
 )
@@ -15,7 +20,32 @@ func main() {
 	r.HandleFunc("/currency-service/health", currencyService.HealthCheck)
 	r.HandleFunc("/currency-service/{from}/to/{to}", currencyService.GetExchangeRate)
 	http.Handle("/", r)
-	log.Println("Starting service...")
-	http.ListenAndServe(":8080", r)
 
+	var wait time.Duration
+	flag.DurationVar(&wait, "graceful-timeout", time.Second*15, "the duration for which the server gracefully wait for existing connections to finish - e.g. 15s or 1m")
+	flag.Parse()
+
+	srv := &http.Server{
+		Addr:         "0.0.0.0:8080",
+		WriteTimeout: time.Second * 15,
+		ReadTimeout:  time.Second * 15,
+		IdleTimeout:  time.Second * 60,
+		Handler:      r,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	<-c
+
+	ctx, cancel := context.WithTimeout(context.Background(), wait)
+	defer cancel()
+	srv.Shutdown(ctx)
+	log.Println("shutting down")
+	os.Exit(0)
 }
